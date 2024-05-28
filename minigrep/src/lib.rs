625bcffcea05ee035f390b65028f6cc71a8db0f8
smontes2 <smontes2@binghamton.edu>
2024-05-28 19:39:51 -0400

    Search feature added

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -3,7 +3,10 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.file_path)?;
-    println!("Here is the file you provided: {contents}");
+    
+    for line in search(&config.query, &contents){
+        println!("{line}");
+    }
 
     Ok(())
 }
@@ -23,4 +26,32 @@ impl Config{
 
         Ok(Config {query, file_path})
     } 
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut results = Vec::new();
+
+    for line in contents.lines(){
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
